79cd2140ce6c5e433bff17c5078a1b5d6cc060f6
Jorge Casariego <jorgekasa@gmail.com>
2016-06-01 16:42:17 -0400

    Segunda Parte del proyecto

--- app/src/main/java/casariego/jorge/androiddesignproject/MainActivity.java
+++ app/src/main/java/casariego/jorge/androiddesignproject/MainActivity.java
@@ -1,8 +1,13 @@
 package casariego.jorge.androiddesignproject;
 
 import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -10,17 +15,22 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import casariego.jorge.androiddesignproject.fragments.ConfiguracionesActivity;
 import casariego.jorge.androiddesignproject.fragments.Fragment1;
 import casariego.jorge.androiddesignproject.fragments.Fragment2;
 import casariego.jorge.androiddesignproject.fragments.Fragment3;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private TabLayout tabLayout;
+    private FloatingActionButton floatingActionButton;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
 
 
 
@@ -38,49 +48,45 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
 
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        navigationView.setNavigationItemSelectedListener(this);
+
+        /*tabLayout = (TabLayout) findViewById(R.id.appbar_tabs);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.addTab(tabLayout.newTab().setText("Estrenos"));
+        tabLayout.addTab(tabLayout.newTab().setText("En Cartelera"));
+        tabLayout.addTab(tabLayout.newTab().setText("50 %"));*/
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floating_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                boolean fragmentTransaction = false;
-                Fragment fragment = null;
-
-                switch (item.getItemId()){
-                    case R.id.menu_seccion_1:
-                        fragment = new Fragment1();
-                        fragmentTransaction = true;
-                        break;
-                    case R.id.menu_seccion_2:
-                        fragment = new Fragment2();
-                        fragmentTransaction = true;
-                        break;
-                    case R.id.menu_seccion_3:
-                        fragment = new Fragment3();
-                        fragmentTransaction = true;
-                        break;
-                    case R.id.menu_opcion_1:
-                        Intent i = new Intent(MainActivity.this, ConfiguracionesActivity.class);
-                        startActivity(i);
-                        break;
-                    case R.id.menu_opcion_2:
-                        Log.d("Navigation View", "Salir");
-                        break;
-                }
-
-                if(fragmentTransaction){
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.content_frame, fragment)
-                            .commit();
-
-                    item.setChecked(true);
-                    getSupportActionBar().setTitle(item.getTitle());
-                }
-
-                drawerLayout.closeDrawers();
-
-                return true;
+            public void onClick(View v) {
+                Snackbar.make(v, "Utilizando Floating Button!", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Cerrar", mSnackBarClickListener)
+                        .show();
             }
         });
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setTitle("Paseo Galería Cine World");
+        collapsingToolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, R.color.colorAccent));
+        collapsingToolbarLayout.setCollapsedTitleTextColor(ContextCompat.getColor(this, android.R.color.white));
+
+        //Hacemos que la primera vez ya cargue el fragment 1
+        Fragment fragment = null;
+        fragment = new Fragment1();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+
     }
+
+    private View.OnClickListener mSnackBarClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Click sobre SnackBar", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -91,6 +97,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        boolean fragmentTransaction = false;
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+            case R.id.menu_seccion_1:
+                fragment = new Fragment1();
+                fragmentTransaction = true;
+                break;
+            case R.id.menu_seccion_2:
+                fragment = new Fragment2();
+                fragmentTransaction = true;
+                break;
+            case R.id.menu_seccion_3:
+                fragment = new Fragment3();
+                fragmentTransaction = true;
+                break;
+            case R.id.menu_opcion_1:
+                Intent i = new Intent(MainActivity.this, ConfiguracionesActivity.class);
+                startActivity(i);
+                break;
+            case R.id.menu_opcion_2:
+                Log.d("Navigation View", "Salir");
+                break;
+        }
+
+        if(fragmentTransaction){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
+        }
+
+        drawerLayout.closeDrawers();
+
+        return true;
     }
 }
 
--- app/src/main/java/casariego/jorge/androiddesignproject/fragments/Fragment1.java
+++ app/src/main/java/casariego/jorge/androiddesignproject/fragments/Fragment1.java
@@ -4,6 +4,7 @@ package casariego.jorge.androiddesignproject.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
